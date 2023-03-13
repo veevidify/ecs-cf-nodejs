442af481a775273e282ab8ed300a8d27cc70a421
V N <vee.vidify@gmail.com>
2023-03-14 06:34:43 +1100

    change health endpoint

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import config from "./config";
 
 const app = createExpressApp();
 app.use("/api", apis);
-app.get("/health", (req, res) => res.send("OK"));
 app.listen(config.port, () => {
   log("info", `[${config.env}] App is listening on port ${config.port}`);
 });
