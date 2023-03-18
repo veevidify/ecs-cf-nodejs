f1aff15feaaccfd18ebb78a1b5a7dffc2babb192
V N <vee.vidify@gmail.com>
2023-03-19 09:20:20 +1100

    include some test for green pipeline

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
-import createExpressApp from "express";
-import { apis } from "./route-controllers";
 import { log } from "./logging";
 import config from "./config";
+import { app } from "./app";
 
-const app = createExpressApp();
-app.use("/api", apis);
 app.listen(config.port, () => {
   log("info", `[${config.env}] App is listening on port ${config.port}`);
 });
